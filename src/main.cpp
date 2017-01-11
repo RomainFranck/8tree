f0884fb1af4efe3c544e72190b7021830cfbbe39
Romain Franck <romain.franck@epitech.eu>
2017-01-11 15:09:05 +0100

    fixed tree size and leaves index

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,19 @@
 
 int main (int argc, char **argv)
 {
-	EigthTree<Voxel> vTree(3, new Voxel(0));
+	EigthTree<Voxel> vTree(8, new Voxel(0));
 
-	vTree.setValue(0b010, 0b100, 0b001, new Voxel(12));
+	for (int i = 0 ; i < 0xff)
+	{
+		for (int j = 0 ; i < 0xff)
+		{
+			for (int k = 0 ; i < 0xff)
+			{
+				vTree.setValue(i, j, k, new Voxel(i << 16 | j << 8 | k));
+			}
+		}
+	}
 
 	std::cout << vTree.getValue(0b011, 0b101, 0b000).color << std::endl;
-	std::cout << vTree.getValue(0b010, 0b100, 0b001).color << std::endl;
+
 }

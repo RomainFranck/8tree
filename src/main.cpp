49bc5f4905c7938f8183caa1e12f56c17cf4add4
Romain Franck <romain.franck@epitech.eu>
2017-01-13 13:48:00 +0100

    clean commit

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,13 @@ int main (int argc, char **argv)
 
 	std::shared_ptr<EightTree<Voxel>> vTree = std::make_shared<EightTree<Voxel>>(size, Voxel(0));
 
-	for (int i = 0 ; i < ((1 << (size + 1)) - 1) ; i++)
+	for (int i = 0 ; i < ((1 << (size + 1))) ; i++)
 	{
-		for (int j = 0 ; j < ((1 << (size + 1)) - 1) ; j++)
+		for (int j = 0 ; j < ((1 << (size + 1))) ; j++)
 		{
-			for (int k = 0 ; k < ((1 << (size + 1)) - 1) ; k++)
+			for (int k = 0 ; k < ((1 << (size + 1))) ; k++)
 			{
-				vTree->setValue(i, j, k, Voxel((i & 6) ? 1 : 2));
+				vTree->setValue(i, j, k, Voxel(j & 3 ? 1 : 0));
 			}
 		}
 	}
